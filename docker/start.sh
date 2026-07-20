a3c075e8d104bca64f7c7a1795aa2e1d09560ec1
Kapilan S D <Kapilan357@gmail.com>
2026-07-20 19:39:09 +0530

    Run migrations on container startup

--- docker/start.sh
+++ docker/start.sh
@@ -7,6 +7,9 @@ php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
+echo "Running database migrations..."
+php artisan migrate --force
+
 php-fpm -D
 
 nginx -g "daemon off;"
